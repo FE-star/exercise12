1df4b3e7461bfd696c83e9d2dd96e17efe8a274e
Ivan <wcy5211678@163.com>
2018-03-04 21:14:38 +0800

    loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.js'
+    path: path.join(__dirname, "dist"),
+    filename: "index.js"
   },
   module: {
     rules: [
       {
         test: /\.js/,
         use: [
-          'define-loader'
+          // 'define-loader'
+          {
+            loader: path.resolve(__dirname, "src/loader.js"),
+          }
         ]
       }
     ]
   },
   resolveLoader: {
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'loader')
-    ]
+    modules: ["node_modules", path.resolve(__dirname, "src/loader")]
   }
-}
+};
