16f32a85b0408b3d34ab6d15ef3684b1452c992a
Eating Lao <eating_miao@163.com>
2018-03-07 12:04:30 +0800

    eating's exercise12

---
+++ loader/define-loader.js
@@ -0,0 +1,9 @@
+module.exports = function(source) {
+    if(this.request.indexOf('webpack/buildin/global.js') > -1) {
+        return source
+    } else {
+        return `global.define(function(module, exports) {
+           ${source}
+        })`
+    }
+}
